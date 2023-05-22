be2705ddd4fc9a31e8191d9a6e7f66af89eb1367
andrea.fan <andrea.fan@ringus-solution.com>
2023-05-22 23:25:46 +0800

    Change layout and styling

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -12,12 +12,12 @@ export default async function Page({
 
   return (
     <main className="w-full relative">
-      <span className="border-[1px] border-[#333] h-[30rem] top-[21px] right-[170px] absolute -z-10"></span>
+      <span className="md:border-[1px] md:border-[#333] md:h-[30rem] md:top-[21px] md:right-[170px] md:absolute md:-z-10"></span>
       <section className="max-w-5xl mx-auto mt-16 flex flex-col items-start  gap-10 px-6">
         <h1 className="text-9xl font-extrabold bg-gradient-to-r from-[#222]  to-[#444] bg-clip-text text-transparent">
           {d['greeting'].goal}
         </h1>
-        <span className="absolute w-2/4 h-7 bg-[#fadc49] -z-20 top-[6rem]"></span>
+        <span className="md:absolute md:w-2/4 md:h-7 md:bg-[#fadc49] md:-z-20 md:top-[6rem]"></span>
         <article className="flex flex-col gap-4">
           <div>
             <h2 className="text-2xl font-semibold text-[#333]">
--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -26,13 +26,13 @@ export default function Card({
   }, [routes, dictionary]);
 
   return (
-    <section className="grid grid-cols-3 gap-5">
+    <section className="grid grid-cols-1 gap-5 px-4 md:grid-cols-3">
       {translateRoutes.map(route => (
         <Link
           href={`/en/apg/${route.url}`}
           key={route.title}
           className="w-full h-20 mb-6 rounded-[.2rem] shadow-lg hover:transform hover:-translate-y-2 transition duration-300 ease-in-out
-          bg-[#343B7B] hover:bg-[#1c2550] flex items-center gap-4 p-4 relative focus:outline-none focus:ring-4 focus:ring-pink-700 focus:border-pink-700"
+          bg-[#333] hover:bg-[#777] flex items-center gap-4 p-4 relative focus:outline-none focus:ring-4 focus:ring-pink-700 focus:border-pink-700"
         >
           <div>
             <BsArrowRightCircle color="white" size="1.2rem" />
@@ -42,7 +42,7 @@ export default function Card({
             <h2 className="text-lg text-white font-bold">{route.title}</h2>
           </div>
 
-          <span className="absolute bg-pink-300 w-5 h-20 right-0 rounded-tr-[0.2rem] rounded-br-[0.2rem]"></span>
+          <span className="absolute bg-yellow-400 w-5 h-20 right-0 rounded-tr-[0.2rem] rounded-br-[0.2rem]"></span>
         </Link>
       ))}
     </section>
