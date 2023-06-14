3011c8b67c26106c32bb776d7fec40e64ed7bb30
andrea.fan <andrea.fan@ringus-solution.com>
2023-06-14 23:52:32 +0800

    Update:Adding accordion

--- app/[lang]/aria/page.tsx
+++ app/[lang]/aria/page.tsx
@@ -54,14 +54,14 @@ export default async function Page({ params }: { params: { lang: Locale } }) {
 
   return (
     <Container>
-      <section className="text-[#444] flex flex-col">
+      <section className="text-[#444] flex flex-col mx-3">
         <h1 className="p-2 text-3xl font-medium mb-5 text-[#fafafa] bg-slate-700 w-fit rounded-md shadow-md">
           {d['aria'].readFirst}
         </h1>
         <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-gray-800 to-gray-600 font-bold text-6xl">
           {d['aria'].noAriaIsBetterThanBadAria}
         </h2>
-        <span className="md:absolute md:w-[50%] md:h-7 md:bg-[#fadc49] md:-z-20 md:top-[13.5rem]"></span>
+        <span className="md:absolute lg:w-[91%] xl:w-[72%] 2xl:w-[49%] md:h-7 md:bg-[#fadc49] md:-z-20 md:top-[13.5rem]"></span>
       </section>
       <ContentCard content={contentData} />
     </Container>
--- app/types/ariaTypes.ts
+++ app/types/ariaTypes.ts
@@ -3,3 +3,5 @@ export interface ContentProps {
   snippets?: string;
   descriptions?: string[];
 }
+
+
